a663cea7530fd8f27e71a0933b21495cc0a36161
ChandrakantNaik <chandrakant.rvce@gmail.com>
2023-03-29 15:52:49 +0000

    disabled logging

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -79,11 +79,11 @@ cors = CORS(
 )
 
 
-@app.after_request
-def after_request(response):
-    timestamp = strftime('[%Y-%b-%d %H:%M]')
-    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
-    return response
+# @app.after_request
+# def after_request(response):
+#     timestamp = strftime('[%Y-%b-%d %H:%M]')
+#     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+#     return response
 
 
 @app.route("/api/message_groups", methods=['GET'])
